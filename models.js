a31cd9aa6eb15905f69fd3dbecb72c52775739f9
Camryn Pearson <camrynpearson@gmail.com>
2018-11-12 16:55:36 -0500

    update

--- models.js
+++ models.js
@@ -3,4 +3,34 @@ const db = new Sequelize("postgres://localhost:5432/plantr", {
   // logging: false;
 })
 
+const Gardener = db.define("gardener", {
+  name: {
+    type: Sequelize.STRING
+  },
+  age: {
+    type: Sequelize.INTEGER
+  }
+})
+
+const Plot = db.define("plot", {
+  size: {
+    type: Sequelize.INTEGER
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN
+  }
+})
+
+const Vegetable = db.define("vegetable", {
+  name: {
+    type: Sequelize.STRING
+  },
+  color: {
+    type: Sequelize.STRING
+  },
+  planted_on: {
+    type: Sequelize.DATE
+  }
+})
+
 module.exports = db;
